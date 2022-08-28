755e77acc1a878989cf7cfd6466692923fc05d8f
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-08-27 17:15:23 -0700

    ✅ The print caused time limit to exceed. Also I cleaned the code.

--- evaluate_reverse_polish_notation.py
+++ evaluate_reverse_polish_notation.py
@@ -4,48 +4,27 @@ class Solution(object):
         :type tokens: List[str]
         :rtype: int
         """
+        if len(tokens) == 1:
+            return tokens[0]
         operator_set = {"+", "-", "*", "/"}
         stack = []
         total = 0
-        if len(tokens) == 1:
-            return tokens[0]
         for i, x in enumerate(tokens):
             if x not in operator_set:
                 stack.append(x)
             elif x in operator_set:
+                total = 0
                 if x == "+":
-                    total = 0
-                    print(total)
-                    print(stack)
                     total += int(stack[-1]) + int(stack[-2])
-                    stack.pop(-1)
-                    stack.pop(-1)
-                    stack.append(total)
                 elif x == "-":
-                    total = 0
-                    print(total)
-                    print(stack)
                     total += int(stack[-2]) - int(stack[-1])
-                    stack.pop(-1)
-                    stack.pop(-1)
-                    stack.append(total)
                 elif x == "*":
-                    total = 0
-                    print(total)
-                    print(stack)
                     total += int(float(stack[-1]) * float(stack[-2]))
-                    stack.pop(-1)
-                    stack.pop(-1)
-                    stack.append(total)
                 elif x == "/":
-                    total = 0
-                    print(total)
-                    print(stack)
-                    print(x)
                     total += int(float(stack[-2]) / float(stack[-1]))
-                    stack.pop(-1)
-                    stack.pop(-1)
-                    stack.append(total)
+                stack.pop(-1)
+                stack.pop(-1)
+                stack.append(total)
 
         print("total is", total)
         return total
