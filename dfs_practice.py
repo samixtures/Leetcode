f8311ee96d4ffdbf979ea9e84d9fccd082e183a6
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-09-22 10:41:05 -0700

    Added another dfs

--- dfs_practice.py
+++ dfs_practice.py
@@ -21,3 +21,27 @@ def dfs(root: TreeNode):
 # .pop() removes from right side (i didn't realize that lol)
 
 dfs(t)
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# t = TreeNode(4, TreeNode(2),  TreeNode(7, TreeNode(6), TreeNode(8)))
+
+# def dfs(t, value):
+#     s = [t]
+#     while s:
+#         node = s.pop(0)
+#         # print(node.val)
+#         if node.val == value:
+#             return node
+#         if node.left:
+#             s.insert(0, node.left)
+#         if node.right:
+#             s.insert(0, node.right)
+#         for x in s:
+#             print(x.val, end =" ")
+#         print("")
+#     return 
+# dfs(t, 2)
