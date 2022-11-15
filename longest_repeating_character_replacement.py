ec1e4635ca76cda858bc33db2cdf7fbee05e76b5
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-11-15 15:01:16 -0800

    ✅ eh kinda memorized...

---
+++ longest_repeating_character_replacement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        h = {}
+        res = 0
+        for r in range(len(s)):
+            h[s[r]] = 1 + h.get(s[r], 0)
+            while (r-l+1) - max(h.values()) > k:
+                h[s[l]] -= 1
+                l += 1
+            res = max(res, r-l+1)
+        return res
