2f1c630edd904137e367cf07165e8d7cf3a1420d
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-05-24 18:01:41 -0700

    ✅ yay🙂

--- kokoEatingBananas.py
+++ kokoEatingBananas.py
@@ -13,16 +13,13 @@ class Solution:
 
         left, right = 0, max(piles)
         mid = (left+right)//2
+        lowestValid = float('inf')
 
         hours = 0
 
 
         while left <= right:
             mid = (left+right)//2
-            
-            # to determine how many hours it will take for Koko to eat each pile
-            # given banans-per-hour eating speed of mid, we must do something...
-            # 
             hours = 0
             
             if mid!= 0:
@@ -31,38 +28,80 @@ class Solution:
                         hours += x//mid
                     else:
                         hours += x//mid + 1
-            
-            if hours < h:
+                print("hours is in 1st while", hours)
+                print("mid in 1st while", mid)
+            # if hours == h:
+            #     break
+            if hours <= h and hours > 0:
+                lowestValid = min(lowestValid, mid)
+            if hours <= h:
                 right = mid - 1
             elif hours > h:
                 left = mid + 1
-            else:
-                right -= 1
+            # else:
+            #     right -= 1
+        print("lowestValid is", lowestValid)
+        print("mid is finally", mid)
+        if lowestValid != float('inf'):
+            mid = max(lowestValid, mid)
+
+        # hours = 0
+        # if mid != 0:
+        #     for x in piles:
+        #         if x%mid == 0:
+        #             hours += x//mid
+        #         else:
+        #             hours += x//mid + 1
+        # print("hours is", hours)
+
+        # while hours > h:
+        #     print("we run?")
+        #     mid += 1
+        #     hours = 0
+        #     if mid != 0:
+        #         for x in piles:
+        #             hours += x//mid + 1
+        #     if hours > h:
+        #         print("we are continuing")
+        #         print("hours is", hours)
+        #         continue
+        #     else:
+        #         break
+
+            # if hours <= h:
+            #     right = mid - 1
+            # elif hours > h:
+            #     left = mid + 1
+            # else:
+            #     right -= 1
 
 
-        hours = 0
-        if mid != 0:
-            for x in piles:
-                if x%mid == 0:
-                    hours += x//mid
-                else:
-                    hours += x//mid + 1
+        # hours = 0
+        # if mid != 0:
+        #     for x in piles:
+        #         if x%mid == 0:
+        #             hours += x//mid
+        #         else:
+        #             hours += x//mid + 1
         print("hours is", hours)
 
-        while hours > h:
-            print("we run?")
-            mid += 1
-            hours = 0
-            if mid != 0:
-                for x in piles:
-                    hours += x//mid + 1
-            if hours > h:
-                print("we are continuing")
-                print("hours is", hours)
-                continue
-            else:
-                # mid -= 1
-                break
+        # while hours < h:
+        #     print("we run?")
+        #     mid -= 1
+        #     hours = 0
+        #     if mid != 0:
+        #         for x in piles:
+        #             hours += x//mid + 1
+        #     if hours > h:
+        #         print("we are continuing")
+        #         print("hours is", hours)
+        #         continue
+        #     else:
+        #         break
+
+
+
         if mid == 0:
             return 1
+
         return mid
