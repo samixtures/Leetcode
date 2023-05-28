8d75bf8ca335b0e5409c8dab84df74e172e8f4d6
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-05-28 08:08:28 -0700

    ✅🙂

--- permutationInString.py
+++ permutationInString.py
@@ -1,67 +1,50 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-                    
-        # EDGE CASES
-
         if len(s1) == 1:
             if s1 in s2:
                 return True
             return False
-
-        if len(s2) > 1000:
+        
+        if len(s1) > len(s2):
             return False
 
-        # if "ba" in s2:
-        #     return True
-        
-        # EDGE CASES
+        firstFreqMap = {
+            "a":0, "b":0, "c":0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, "k":0, "l":0, "m":0, 
+            "n":0, "o":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0, "v":0, "w":0, "x":0, "y":0, "z":0
+        }
 
-        freqMap = {}
         for x in s1:
-            if x in freqMap:
-                freqMap[x] += 1
-            else:
-                freqMap[x] = 1
-        print("freqMap is", freqMap)
+            firstFreqMap[x] += 1
+        
+        """
+        sliding window frequency map for s2
+        the window should be the size of len(s1)
+        """
 
-        permSet = set()
+        l, r = 0, 1
+        secondFreqMap = {
+            "a":0, "b":0, "c":0, "d":0, "e":0, "f":0, "g":0, "h":0, "i":0, "j":0, "k":0, "l":0, "m":0, 
+            "n":0, "o":0, "p":0, "q":0, "r":0, "s":0, "t":0, "u":0, "v":0, "w":0, "x":0, "y":0, "z":0
+        }
+        secondFreqMap[s2[l]] = 1
 
-        for i in range(len(s1)):
-            permSet.add(s1[i])
+        while (r-l) < len(s1):
+            secondFreqMap[s2[r]] += 1
+            r += 1
+        # print("secondFreqMap:", secondFreqMap)
+        while r < len(s2):
+            if secondFreqMap == firstFreqMap:
+                return True
+            secondFreqMap[s2[l]] -= 1
+            l += 1
+            secondFreqMap[s2[r]] += 1
+            r+= 1
+        #     print("secondFreqMap:", secondFreqMap)
 
+        # print("firstFreqMap:", firstFreqMap)
+        # print("secondFreqMap:", secondFreqMap)
 
-        for i in range(len(s2)):
-            permString = ""
-
-            if s2[i] in permSet:
-                left = i
-                counter = 1
-                permString += s2[left]
-                compareFreqMap = {}
-                compareFreqMap[s2[left]] = 1
-
-                while counter < len(s1) and left < len(s2)-1:
-                    left += 1
-                    if s2[left] in permSet:
-                        if s2[left] not in compareFreqMap:
-                            compareFreqMap[s2[left]] = 1
-                            permString += s2[left]                    
-                            counter += 1
-                        elif s2[left] in compareFreqMap and compareFreqMap[s2[left]] < freqMap[s2[left]]:
-                            compareFreqMap[s2[left]] += 1
-                            permString += s2[left]                    
-                            counter += 1
-                        elif compareFreqMap[s2[left]] == freqMap[s2[left]]:
-                            counter += 1
-                        else:
-                            counter += 1
-                    else:
-                        break
-                print("compareFreqMap:", compareFreqMap)
-                print("permString:", permString)
-
-                if len(permString) == len(s1):
-                    return True 
-
+        if secondFreqMap == firstFreqMap:
+            return True
 
         return False
