f1aca21b262cc31b61024d823c9736440e2c326b
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-10-07 15:17:04 -0700

    had to look at solution, I should redo it eventually

---
+++ pathSum.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        currentSum = 0
+        def dfs(root, currentSum):
+            if not root:
+                return False
+            currentSum += root.val
+            if not root.left and not root.right:
+                if currentSum == targetSum:
+                    return True
+                else: 
+                    return False
+            return dfs(root.left, currentSum) or dfs(root.right, currentSum)
+            
+
+        return dfs(root, currentSum)
